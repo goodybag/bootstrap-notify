5a486e9921bd7fcc96e0ba45a6d0a27b03a0e201
Nijiko Yonskai <nijikokun@gmail.com>
2012-12-24 15:21:24 -0800

    Add support for data-attributes

--- js/bootstrap-notify.js
+++ js/bootstrap-notify.js
@@ -1,7 +1,8 @@
 /**
- * bootstrap-notify.js v1.0
+ * bootstrap-notify.js v1.0.0
  * --
-  * Copyright 2012 Goodybag, Inc.
+ * Copyright 2012 Nijiko Yonskai <nijikokun@gmail.com>
+ * Copyright 2012 Goodybag, Inc.
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +22,7 @@
     // Element collection
     this.$element = $(element);
     this.$note    = $('<div class="alert"></div>');
-    this.options  = $.extend(true, {}, $.fn.notify.defaults, options);
+    this.options  = $.extend(true, $.fn.notify.defaults, options, this.$element.data());
 
     // Setup from options
     if(this.options.transition)
@@ -34,19 +35,17 @@
       this.$note.addClass('alert-' + this.options.type);
     else this.$note.addClass('alert-success');
 
-    if(!this.options.message && this.$element.data("message") !== '') // dom text
-      this.$note.html(this.$element.data("message"));
-    else 
-      if(typeof this.options.message === 'object')
+    if(this.options.message)
+      if(typeof this.options.message === 'string')
+        this.$note.html(this.options.message);
+      else if(typeof this.options.message === 'object')
         if(this.options.message.html)
           this.$note.html(this.options.message.html);
         else if(this.options.message.text)
           this.$note.text(this.options.message.text);
-      else 
-        this.$note.html(this.options.message);
 
     if(this.options.closable)
-      var link = $('<a class="close pull-right" href="#">&times;</a>');
+      var link = $('<a class="close pull-right">&times;</a>');
       $(link).on('click', $.proxy(onClose, this));
       this.$note.prepend(link);
 
