a626105766bf576c9ac818708ad8ec8f62d26681
Nijiko Yonskai <nijikokun@gmail.com>
2013-01-31 23:13:41 -0800

    Update js/bootstrap-notify.js
    Updated to follow parsify yolo guidelines, and fix closing

--- js/bootstrap-notify.js
+++ js/bootstrap-notify.js
@@ -23,43 +23,44 @@
     this.$element = $(element);
     this.$note    = $('<div class="alert"></div>');
     this.options  = $.extend(true, $.fn.notify.defaults, options, this.$element.data());
+    this._link    = null;
 
     // Setup from options
-    if(this.options.transition)
-      if(this.options.transition == 'fade')
+    if (this.options.transition)
+      if (this.options.transition == 'fade')
         this.$note.addClass('in').addClass(this.options.transition);
       else this.$note.addClass(this.options.transition);
     else this.$note.addClass('fade').addClass('in');
 
-    if(this.options.type)
+    if (this.options.type)
       this.$note.addClass('alert-' + this.options.type);
     else this.$note.addClass('alert-success');
 
-    if(this.options.message)
-      if(typeof this.options.message === 'string')
+    if (this.options.message)
+      if (typeof this.options.message === 'string')
         this.$note.html(this.options.message);
-      else if(typeof this.options.message === 'object')
-        if(this.options.message.html)
+      else if (typeof this.options.message === 'object')
+        if (this.options.message.html)
           this.$note.html(this.options.message.html);
-        else if(this.options.message.text)
+        else if (this.options.message.text)
           this.$note.text(this.options.message.text);
 
-    if(this.options.closable)
-      var link = $('<a class="close pull-right">&times;</a>');
-      $(link).on('click', $.proxy(onClose, this));
-      this.$note.prepend(link);
+    if (this.options.closable)
+      this._link = $('<a class="close pull-right">&times;</a>'),
+      $(this._link).on('click', $.proxy(Notification.onClose, this)),
+      this.$note.prepend(this._link);
 
     return this;
   };
 
-  onClose = function() {
+  Notificaiton.onClose = function () {
     this.options.onClose();
     $(this.$note).remove();
     this.options.onClosed();
   };
 
   Notification.prototype.show = function () {
-    if(this.options.fadeOut.enabled)
+    if (this.options.fadeOut.enabled)
       this.$note.delay(this.options.fadeOut.delay || 3000).fadeOut('slow', $.proxy(onClose, this));
 
     this.$element.append(this.$note);
@@ -67,7 +68,7 @@
   };
 
   Notification.prototype.hide = function () {
-    if(this.options.fadeOut.enabled)
+    if (this.options.fadeOut.enabled)
       this.$note.delay(this.options.fadeOut.delay || 3000).fadeOut('slow', $.proxy(onClose, this));
     else onClose.call(this);
   };
